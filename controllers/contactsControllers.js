789613f0404abc2418fe3cdc356443104626a321
HannaMelnyk2023 <151830548+HannaMelnyk2023@users.noreply.github.com>
2026-03-29 00:53:29 +0200

    add contactControllers

--- controllers/contactsControllers.js
+++ controllers/contactsControllers.js
@@ -1,11 +1,49 @@
 import contactsService from "../services/contactsServices.js";
+import HttpError from "../helpers/HttpError.js";
 
-export const getAllContacts = (req, res) => {};
 
-export const getOneContact = (req, res) => {};
+export const getAllContacts = (req, res) => {
+    contactsService.listContacts().then((contacts) => res.json(contacts));
+};
 
-export const deleteContact = (req, res) => {};
+export const getOneContact = (req, res) => {
+    const { id } = req.params;
+    contactsService.getContactById(id).then((contact) => {
+        if (!contact) {
+            return res.status(404).json({ message: "Contact not found" });
+        }
+        res.json(contact);
+    });
+};
 
-export const createContact = (req, res) => {};
+export const deleteContact = (req, res) => {
+    const { id } = req.params;
+    contactsService.removeContact(id).then((contact) => {
+        if (!contact) {
+            return res.status(404).json({ message: "Contact not found" });
+        }
+        res.json({ message: "Contact deleted" });
+    });
+};
 
-export const updateContact = (req, res) => {};
+export const createContact = (req, res) => {
+    const { name, email, phone } = req.body;
+    contactsService.addContact(name, email, phone).then((contact) => {
+        res.status(201).json(contact);
+    });
+};
+
+export const updateContact = (req, res) => {
+    const { id } = req.params;
+    const { name, email, phone } = req.body;
+    contactsService.getContactById(id).then((contact) => {
+        if (!contact) {
+            return res.status(404).json({ message: "Contact not found" });
+        }
+        const updatedContact = { ...contact, name, email, phone };
+        contactsService
+            .removeContact(id)
+            .then(() => contactsService.addContact(updatedContact))
+            .then(() => res.json(updatedContact));
+    });
+};
--- schemas/contactsSchemas.js
+++ schemas/contactsSchemas.js
@@ -1,7 +1,6 @@
 import Joi from "joi";
 
 export const createContactSchema = Joi.object({
-
 })
 
 export const updateContactSchema = Joi.object({
